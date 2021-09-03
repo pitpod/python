ee4338ece6dacef06054b18362bb86203480eaea
matsu <a6m5a@mamaweb.net>
2021-09-03 11:49:04 +0900

    20210903

---
+++ head_tail.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+df = pd.DataFrame(np.random.randn(20,2))
+df.head()
+df.tail()
+
+df.head().append(df.tail())
+df.head(3).append(df.tail(3))
+
+print(df.head(3).append(df.tail(3)))
