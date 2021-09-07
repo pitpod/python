a20d298b2f468401bbdc7185f06d005f9d5878bd
matsu <a6m5a@mamaweb.net>
2021-09-07 11:45:41 +0900

    20210907

--- SNS_data_test.py
+++ SNS_data_test.py
@@ -1,9 +1,21 @@
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv("sns_data.csv",
+
+url = './sns_data.csv'
+df = pd.read_csv(url,
                     encoding='utf8'
                  )
-#df = pd.DataFrame(data, columns=["user_id","follow","follower","like"])
+df.columns = ["user_id","follow","follower","like"]
 
-print(data)
+#print(df)
+
+d1 = {'data1': ['a','b','c','d','c','a'], 'data2': range(6)}
+d1 = pd.DataFrame(d1,index=['one','two','three','four','five','six'])
+
+#d1.to_csv("to_csv.csv",header=False, index=False)
+#d1['data3'] = range(6)
+d2 = pd.DataFrame([['e',10]],index=['seven'],columns=['data1','data2'])
+print(d1.append(d2))
+
+d1.to_excel("to_csv.xlsx")
