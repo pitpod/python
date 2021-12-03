9fed10bb7b4ab502114bc3a3859a977e868a7300
matsu <a6m5a@mamaweb.net>
2021-12-03 11:55:07 +0900

    20211203

--- read_json.py
+++ read_json.py
@@ -7,7 +7,8 @@ import pandas as pd
 import xlwt
 import re
 
-output_contacts = open('./output.vcf', 'w', encoding='utf-8')
+#output_contacts = open('./output.vcf', 'w', encoding='SHIFT-JIS')
+output_contacts = open('./output-3.vcf', 'w', encoding='utf-8')
 folder_path = input("フォルダ名 :")
 files = os.listdir(folder_path)
 # files_file = [f for f in files if os.path.isfile(os.path.join(folder_path, f))]
@@ -17,7 +18,7 @@ for f in files:
         if re.search('\\.json', f):
             files_file = files_file + [f]
 
-print(files_file)   # ['file1', 'file2.txt', 'file3.jpg']
+# print(files_file)   # ['file1', 'file2.txt', 'file3.jpg']
 json_load = []
 for fn in files_file:
     json_file = open(f"{folder_path}/{fn}", encoding='utf-8')
@@ -26,17 +27,18 @@ for fn in files_file:
     # print(len(json_load))
 
 # for i in range(0, 50):
-print(json_load)
+# print(json_load)
 for i in range(0, len(json_load)):
     output_contacts.write('BEGIN:VCARD\n')
     output_contacts.write('VERSION:3.0\n')
+    # output_contacts.write('VERSION:2.1\n')
     firstname = json_load[i]['structuredName']['data1']
     lastname = json_load[i]['structuredName']['data3']
-    output_contacts.write('N:' + lastname + ';' + firstname + '\n')
-    output_contacts.write('FN:' + firstname + ' ' + lastname + '\n')
-    output_contacts.write('X-PHONETIC-LAST-NAME:' + json_load[i]['structuredName']['data9'] + '\n')
-    output_contacts.write('TEL;type=CELL;type=VOICE;type=pref:' + json_load[i]['phone'][0]['data1'] + '\n')
-    output_contacts.write('END:VCARD' + '\n')
+    output_contacts.write(f"N:{lastname};{firstname}\n")
+    output_contacts.write(f"FN:{firstname} {lastname}\n")
+    output_contacts.write(f"X-PHONETIC-LAST-NAME:{json_load[i]['structuredName']['data9']}\n")
+    output_contacts.write(f"TEL;type=CELL;type=VOICE;type=pref:{json_load[i]['phone'][0]['data1']}\n")
+    output_contacts.write('END:VCARD\n')
 
 json_file.close()
 output_contacts.close()
