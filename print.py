ecd23080457077ae7c925871a7f2027ff90c99a9
幸治 松本 <pitpod@logue.onmicrosoft.com>
2022-02-02 18:30:47 +0900

    20220202

--- print.py
+++ print.py
@@ -7,6 +7,8 @@ import tkinter as tk
 import sys
 # import os
 import io
+from io import StringIO
+from io import BytesIO
 import tempfile
 import subprocess
 from PIL import ImageFont, ImageDraw, Image
@@ -32,19 +34,70 @@ class Application(tk.Frame):
         root.geometry("800x480")
         button = tk.Button(root, text="閉じる", command=sys.exit)
         button.pack()
-        # button2 = tk.Button(root, text="印刷", command=lambda: self.print_paper(sentence))
-        button2 = tk.Button(root, text="印刷", command=self.printer)
+        button1 = tk.Button(root, text="印刷1", command=lambda: self.print_paper(sentence))
+        button1.pack()
+        button2 = tk.Button(root, text="印刷2", command=self.printer)
         button2.pack()
         button3 = tk.Button(root, text="印刷3", command=self.use_win32print)
         button3.pack()
         button4 = tk.Button(root, text="印刷4", command=self.use_ShellExecute)
         button4.pack()
+        button5 = tk.Button(root, text="イメージテキスト", command=self.image_text)
+        button5.pack()
         # root.mainloop()
+        """[summary]
+        印刷1
+        """
+    def print_paper(self, sentence):
+        print(sentence)
+        self.sentence = sentence
+        self.INCH = 1440
+        self.SCALE_FACTOR = 20
+
+        fontdict = {
+            "name": "ＭＳ 明朝",
+            "height": self.SCALE_FACTOR * 40,
+            "charset": win32con.SHIFTJIS_CHARSET,
+        }
+        self.font = win32ui.CreateFont(fontdict)
+
+        self.hDC = win32ui.CreateDC()
+        self.hDC.CreatePrinterDC(win32print.GetDefaultPrinter())
+
+        self.oldfont = self.hDC.SelectObject(self.font)
+
+        self.hDC.StartDoc('TestPrint')
+        self.hDC.StartPage()
+        self.hDC.SetMapMode(win32con.MM_TWIPS)
+        self.hDC.DrawText(self.sentence, (0, self.INCH * -1, self.INCH * 8, self.INCH * -2), win32con.DT_CENTER)
+        self.hDC.SetTextAlign(win32con.TA_LEFT | win32con.TA_TOP | win32con.TA_NOUPDATECP)
+        self.hDC.TextOut(0, self.SCALE_FACTOR * 10 * -1, self.sentence)
+        self.hDC.SelectObject(self.oldfont)
+        self.hDC.EndPage()
+        self.hDC.EndDoc()
+
+    """button2"""
+    def printer(self, print_text=""):
+        printers = win32print.EnumPrinters(2)
+        for p in printers:
+            print(p)
+
+        buf = BytesIO()
+        # buf = StringIO()
+        screen = (500, 500)
+        bgcolor = (0x00, 0x00, 0x00)
+        img = Image.new('RGB', screen, bgcolor)
+        img.save(buf, 'PNG')
+        printer_name = win32print.GetDefaultPrinter()
+        p = subprocess.Popen('print', stdin=subprocess.PIPE)
+        p.communicate(buf.getvalue())
+        p.stdin.close()
+        buf.close()
 
     def use_ShellExecute(self):
         filename = tempfile.mktemp(".txt")
         # ファイルを開くアプリケーションによってはエラーになる
-        open(filename, "w").write("This is a test")
+        open(filename, "w").write("This is a テスト")
         win32api.ShellExecute(
             0,
             "print",
@@ -55,85 +108,29 @@ class Application(tk.Frame):
         )
 
     def use_win32print(self):
-        printer_name = win32print.GetDefaultPrinter()
+        self.printer_name = win32print.GetDefaultPrinter()
         # printer_name = 'Microsoft Print to PDF'
         #
         # raw_data could equally be raw PCL/PS read from
         #  some print-to-file operation
         #
         if sys.version_info >= (3,):
-          raw_data = bytes("This is a test", "utf-8")
+            self.raw_data = bytes("This is a test", "utf-8")
         else:
-          raw_data = "This is a test"
+            self.raw_data = "This is a test"
 
-        print(raw_data)
-        hPrinter = win32print.OpenPrinter(printer_name)
+        print(self.raw_data)
+        self.hPrinter = win32print.OpenPrinter(self.printer_name)
         try:
-          hJob = win32print.StartDocPrinter(hPrinter, 1, ("test of raw data", None, "RAW"))
-          try:
-            win32print.StartPagePrinter(hPrinter)
-            win32print.WritePrinter(hPrinter, raw_data)
-            win32print.EndPagePrinter(hPrinter)
-          finally:
-            win32print.EndDocPrinter(hPrinter)
+            self.hJob = win32print.StartDocPrinter(self.hPrinter, 1, ("test of raw data", None, "RAW"))
+            try:
+                win32print.StartPagePrinter(self.hPrinter)
+                win32print.WritePrinter(self.hPrinter, self.raw_data)
+                win32print.EndPagePrinter(self.hPrinter)
+            finally:
+                win32print.EndDocPrinter(self.hPrinter)
         finally:
-          win32print.ClosePrinter(hPrinter)
-
-
-    def printer(self, print_text=""):
-        printers = win32print.EnumPrinters(2)
-        for p in printers:
-            print(p)
-
-        buf = io.BytesIO()
-        screen = (500, 500)
-        bgcolor = (0x00, 0x00, 0x00)
-        img = Image.new('RGB', screen, bgcolor)
-        img.save(buf, 'PNG')
-        # text = 'print out message'
-        # printer_name = win32print.GetDefaultPrinter()
-        printer_name = 'Microsoft Print to PDF'
-        hPrinter = win32print.OpenPrinter(printer_name)
-        # win32api.ShellExecute(0, 'print', buf.getvalue(), printer_name, '.', 0)
-        p = subprocess.Popen('lp', stdin=subprocess.PIPE)
-        p.communicate(buf.getvalue())
-        # p.communicate(text)
-        p.stdin.close()
-        buf.close()
-
-    def print_paper(self, sentence):
-        print(sentence)
-        self.buf = io.BytesIO()
-        self.screen = (500, 500)
-        self.bgcolor = (0x00, 0x00, 0x00)
-        self.img = Image.new('RGB', self.screen, self.bgcolor)
-        self.img.save(self.buf, 'PNG')
-        self.print_image = self.buf.getvalue()
-        self.buf.close()
-        # self.image_text()
-        self.sentence = sentence
-        fontpath = 'C:\\Windows\\Fonts\\HGRPP1.TTC'
-        font = ImageFont.truetype(fontpath, 32)
-        self.INCH = 1440
-        self.hDC = win32ui.CreateDC()
-        self.hDC.CreatePrinterDC(win32print.GetDefaultPrinter())
-        self.SCALE_FACTOR = 20
-        fontdict = {
-            "height": self.SCALE_FACTOR * 40,
-            "name": "ＭＳ 明朝",
-            "charset": win32con.SHIFTJIS_CHARSET,
-        }
-        self.font = win32ui.CreateFont(fontdict)
-        self.oldfont = self.hDC.SelectObject(self.font)
-        self.hDC.StartDoc('TestPrint')
-        self.hDC.StartPage()
-        self.hDC.SetMapMode(win32con.MM_TWIPS)
-        self.hDC.SetTextAlign(win32con.TA_LEFT | win32con.TA_TOP | win32con.TA_NOUPDATECP)
-        self.hDC.TextOut(0, self.SCALE_FACTOR * 10 * -1, self.sentence)
-        self.hDC.DrawText(self.sentence, (0, self.INCH * -1, self.INCH * 8, self.INCH * -2), win32con.DT_CENTER)
-        self.hDC.SelectObject(self.oldfont)
-        self.hDC.EndPage()
-        self.hDC.EndDoc()
+            win32print.ClosePrinter(self.hPrinter)
 
     def image_text(self):
         img = np.zeros((200, 500, 3), np.uint8)
